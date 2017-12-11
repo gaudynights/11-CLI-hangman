897d1d45eb2863f72bcad408dbcafdcd6ef69503
Michael Balagia <mbalagia@gmail.com>
2017-12-11 17:51:25 -0600

    word js almost works

--- hangman.js
+++ hangman.js
@@ -2,4 +2,6 @@ var inquirer=require("inquirer");
 var answerList=require("./answer-list.js");
 var Word=require("./word.js");
 
+var currentWord="candy";
 
+console.log(Word.print());
--- word.js
+++ word.js
@@ -1,9 +1,40 @@
   // * **Word**: Used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
 
-  var Letter=require("./letter.js");
+  // constructs displayed thing - blanks and letters
+  // check if the letters are in the word
+  // tracks number of guesses
+  //tracks arr of guessed letters
 
-console.log("WORD reporting for duty");
+  var Letter = require("./letter.js");
 
-// var Word=function(){}
+  console.log("WORD reporting for duty");
 
-  // module.exports=Word;
+  var Word = function(currentWord) {
+      this.numGuessesRemaning = 10;
+      this.currentWord = currentWord;
+      this.letters = [];
+      this.guesses = [];
+
+
+      for (var i = 0; i < this.currentWord.length; i++) {
+          this.letters.push(new Letter(this.currentWord[i]));
+      
+      };
+    console.log(this.letters);
+  };
+
+  Word.prototype.print = function() {
+      var display = "";
+      for (var p = 0; p < this.letters.length; p++) {
+          display += this.letters[p].printLetter
+      }
+      return display;
+  };
+
+Word("pizza");
+// function testPrint(word){
+// 	Word.print();
+// };
+// testPrint("candy");
+
+  module.exports = Word;
